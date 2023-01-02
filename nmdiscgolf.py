195a384a0f9948ac474f620a475be4d29422d6d8
jsta <stachel2@msu.edu>
2023-01-02 09:37:29 -0700

    mwe

--- nmdiscgolf.py
+++ nmdiscgolf.py
@@ -1,11 +1,17 @@
 import re
 import twitter
 import requests
+import datetime
 import pandas as pd
+from colorama import Fore
 from bs4 import BeautifulSoup
 
-def get_tourneys():
+import config
 
+tweet = True
+interactive = False
+
+def get_tourneys():
     baseurl = 'https://www.pdga.com/tour/search?OfficialName=&td=&date_filter%5Bmin%5D%5Bdate%5D=2023-01-01&date_filter%5Bmax%5D%5Bdate%5D=2024-01-01&State%5B%5D=NM'
 
     response = requests.get(baseurl)
@@ -30,4 +36,43 @@ def get_tourneys():
     res = pd.DataFrame([names, dates, locations, links]).T
     res.columns = ["name", "date", "location", "link"]
 
+    res = [" | ".join(res.iloc[i]) for i in range(0, res.shape[0])]
+    res = pd.DataFrame(res, columns=["slug"])
+
     return res
+
+
+tourneys = get_tourneys()
+
+log = pd.read_csv("log.csv")
+res = tourneys[~tourneys["slug"].isin(log["slug"])]
+res = res.sample(frac=1)
+
+## print/offer tweets
+slugs = res["slug"]
+print(Fore.GREEN + "Filtered: ")
+print()
+for slug in slugs:    
+    print(Fore.GREEN + slug)
+    print()
+
+if tweet is True or interactive is True:
+    # api = twitter.Api(
+    #     consumer_key=config.api_key,
+    #     consumer_secret=config.api_secret_key,
+    #     access_token_key=config.access_token_key,
+    #     access_token_secret=config.access_token_secret,
+    # )
+    # api.VerifyCredentials()
+
+    for i in range(len(res)):
+        # status = api.PostUpdate(slug)
+        # write to log
+        keys = ["slug", "date"]
+        slug = res.iloc[i]["slug"]
+        date = str(datetime.date.today())        
+        d = dict(zip(keys, [slug, date]))
+
+        d = pd.DataFrame.from_records(d, index=[0])
+        log = log.append(pd.DataFrame(data=d), ignore_index=True)
+        log.to_csv("log.csv", index=False)
